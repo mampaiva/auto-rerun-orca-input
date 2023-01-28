d67cebfc4fe5f63bdfbaa1d50bf2b01ce9462f15
Mateus Paiva <64433509+mampaiva@users.noreply.github.com>
2023-01-27 23:47:34 -0300

    improvised queue

---
+++ queue.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name_input=h2co-forsterita-1-wb97xd3-freq.inp 
+
+./auto-rerun-orca-input $name_input
+
+name_input=h2co-forsterita-2-wb97xd3-freq.inp 
+
+./auto-rerun-orca-input $name_input
